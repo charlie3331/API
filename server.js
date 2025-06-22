31c24e550c53ca9e31b14c5ee9b224a363b7f8d6
charlie3331 <133290015+charlie3331@users.noreply.github.com>
2025-06-22 08:54:39 -0600

    cambios de clave

--- server.js
+++ server.js
@@ -11,14 +11,14 @@ app.use(express.json());
 
 // 🔐 Firebase web config (NO service account)
 const firebaseConfig = {
-  apiKey: "AIzaSyAXOiYtc0W0zfedwKAJaoWNO2CydqGkOo0",
-  authDomain: "gina1-838c7.firebaseapp.com",
-  databaseURL: "https://gina1-838c7-default-rtdb.firebaseio.com",
-  projectId: "gina1-838c7",
-  storageBucket: "gina1-838c7.firebasestorage.app",
-  messagingSenderId: "860331012943",
-  appId: "1:860331012943:web:a6c8105c96dc5a9e4f0b3a",
-  measurementId: "G-NR63G2KPVW"
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
